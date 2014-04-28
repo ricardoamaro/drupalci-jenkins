02a51a8e44e9194078f8e08897a89baf9221cde6
Nick Schuch <nick@myschuch.com>
2014-04-28 14:59:56 +1000

    Update the provision script not to run on all provisions. Adds Docker 0.10.0 for slaves.

--- puppet/provision.sh
+++ puppet/provision.sh
@@ -3,7 +3,24 @@
 # Script: provision.sh
 # Author: Nick Schuch
 
-apt-get -y update
-apt-get -y install curl wget git rubygems vim
-gem install puppet
-apt-get clean
+# Helper function to install packages.
+aptInstall() {
+  COUNT=`dpkg --get-selections $1 | wc -l`
+  if [ "$COUNT" -eq "0" ]; then
+    apt-get -y update > /dev/null
+    apt-get -y install $1
+  fi
+}
+
+# Install the required packages.
+aptInstall curl
+aptInstall wget
+aptInstall git
+aptInstall rubygems
+aptInstall vim
+
+# Ensure we have the latest puppet.
+PUPPET_COUNT=`gem list | grep puppet | wc -l`
+if [ "$PUPPET_COUNT" -eq "0" ]; then
+  gem install puppet
+fi
