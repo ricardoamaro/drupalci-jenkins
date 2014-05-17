f207a0ac701b74fa0b945d12d6a00d20ff8fdb8b
Nick Schuch <nick@myschuch.com>
2014-05-17 14:09:19 +1000

    Remove Ubuntu.

--- puppet/site.pp
+++ puppet/site.pp
@@ -10,14 +10,4 @@ node 'default' {
   # Base classes.
   hiera_include("classes")
 
-  ##
-  # Things that cannot be in hiera.
-  ##
-
-  # Ensure we have an update to date set of packages.
-  exec { "apt-update":
-    command => "/usr/bin/apt-get update"
-  }
-  Exec["apt-update"] -> Package <| |>
-
 }
